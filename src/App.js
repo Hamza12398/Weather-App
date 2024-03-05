b99aeb3b6c9beea8f5cf5458533b47964edc84fb
Hamza12398 <hamzacherkaoui806@gmail.com>
2024-03-05 16:39:34 +0100

    Done with UI Weather App

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <Container maxWidth="sm">
+        <Container maxWidth="sm" >
           {/*  CONTENT CONTAINER  */}
           <div
             style={{
